e5a522e57473f3c82633cdb7c4bde2ec1d546c39
Egrinaldo Junior <egrinaldo19@gmail.com>
2024-03-27 12:30:20 -0300

    adicionado dois teste basicos

--- frontEnd/src/tests/01.NavBar.test.jsx
+++ frontEnd/src/tests/01.NavBar.test.jsx
@@ -1,6 +1,19 @@
-import { expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { NavBar } from "../components/NavBar/NavBar";
+
+describe('VALIDAÇÃO DO COMPONENTE NAVBAR', () => {
+    it('Validando funcionamento do teste', () =>{
+        expect(1).toBe(1);
+    });
+
+    it('Será validado se o componente NavBAr será renderizado', () => {
+        render(<NavBar />)
+    })
 
 
-it('VERIFICAR A RENDERIZAÇÃO', () => {
-    expect(1).toBe(1);
-});
+})
+
+    
+
+
