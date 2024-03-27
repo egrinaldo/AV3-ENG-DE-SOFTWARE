eceead4aef7418325c119921d58c20191c7b55d2
Egrinaldo Junior <egrinaldo19@gmail.com>
2024-03-27 12:09:34 -0300

    first commit

---
+++ frontEnd/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,7 @@
+
+export const NavBar = () => {
+    return(
+        <>
+        <h1>TESTE</h1></>
+    )
+}
---
+++ frontEnd/src/tests/01.NavBar.test.jsx
@@ -0,0 +1,6 @@
+import { expect, it } from "vitest";
+
+
+it('VERIFICAR A RENDERIZAÇÃO', () => {
+    expect(1).toBe(1);
+});
