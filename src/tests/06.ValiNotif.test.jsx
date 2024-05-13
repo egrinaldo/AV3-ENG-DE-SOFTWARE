85b2ec78e7a297ffb26783230561f69934951ed7
Jackson Reis <jreis3607@gmail.com>
2024-05-13 17:25:58 -0300

    test: iniciando configurações do teste 06

--- src/tests/06.ValiNotif.test.jsx
+++ src/tests/06.ValiNotif.test.jsx
@@ -6,8 +6,8 @@
 // Critérios de Sucesso: Uma notificação de confirmação é mostrada para a escola após
 // o registro bem-sucedido.
 
-import { render, getByText } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { render, getByText, getByTestId } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import { Form } from "../components/Form/Form";
 import userEvent from "@testing-library/user-event";
 
@@ -15,7 +15,16 @@ import userEvent from "@testing-library/user-event";
 describe('VERIFICAÇÃO SE O PROCESSO DE NOTIFICAÇÃO ESTA FUNCIONANDO ', () => {
 
     it('Será validado se o componente Form será renderizado', () => {
-       render(<Form />)
+       const { getByText } = render(<Form/>);
+
+       const botaoSalvar = getByTestId('Salvar');
+       userEvent.click(botaoSalvar);
+       expect(getByText("Ocorreu um erro ao enviar os dados.")).toBeInTheDocument();
+
+    //    const { getByTestId, getByText} = render(<Form />)
+    //    userEvent.click(getByTestId('salvar'));
+    //    expect(getByText("Dados enviados com sucesso!")).toBeInTheDocument();
+
     })
 
 
