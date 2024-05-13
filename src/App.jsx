25041678e2eb20587ae0c80744988a8a04c04966
Egrinaldo Junior <egrinaldo19@gmail.com>
2024-05-13 10:27:56 -0300

    test(03): Teste 03 adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Form } from "./components/Form/Form"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { SubmitMock } from "./tests/mock2.1/SubmitMock";
 function App() {
 
   return (
     <>
-    {/* <Form /> */}
-    <SubmitMock />
+    <Form />
     <ToastContainer />
     </>
   )
--- src/tests/03.LimpForm.test.jsx
+++ src/tests/03.LimpForm.test.jsx
@@ -1,13 +1,17 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import {describe, it, expect} from 'vitest';
+import {render, screen, fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ClearMock } from './mock2.1/ClearMock';
 
+describe('VALIDANDO SE AO INSERIR UM DETERMINADO DADO E AO CLICAR NO BOTÃO LIMPAR TUDO SERÁ EXCLUIDO E O INPUT PRONTO PARA INCLUIR NOVOS DADOS',() => {
+    it('VALIDANDO SE AO INSERIR UM DETERMINADO DADO E AO SALVAR O BOTÃO REALMENTE IRÁ SUBMETER O FORMS', async() => {
+        render(<ClearMock />)
 
+        await userEvent.type(screen.getByRole('textbox'), 'escolaTeste');
+        
+        fireEvent.click(screen.getByRole('button', { name: /Limpar/i }));
 
-describe('VERIFICAÇÃO SE O FORMULARIO SERA LIMPO', () => {
+        expect(screen.getByRole('textbox')).toHaveValue('');
 
-    it('', () => {
-        render(<></>)
     })
-
-
 })
