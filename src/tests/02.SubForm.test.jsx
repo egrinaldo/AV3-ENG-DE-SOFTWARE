6f9800c21695fb399dad0cb3fbc6b14d6470f1a4
Egrinaldo Junior <egrinaldo19@gmail.com>
2024-05-13 09:02:27 -0300

    test(02): criação do teste

--- src/tests/02.SubForm.test.jsx
+++ src/tests/02.SubForm.test.jsx
@@ -1,13 +1,22 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import {describe, it, expect} from 'vitest';
+import {render, screen, fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { SubmitMock } from './mock2.1/SubmitMock';
 
+describe('VALIDANDO SE AO INSERIR UM DETERMINADO DADO E AO SALVAR O BOTÃO REALMENTE IRÁ SUBMETER O FORMS',() => {
+    it('VALIDANDO SE AO INSERIR UM DETERMINADO DADO E AO SALVAR O BOTÃO REALMENTE IRÁ SUBMETER O FORMS', async() => {
+        const originalLog = console.log;
+        let consoleOutput = [];
+        console.log = (...output) => consoleOutput.push(...output);
 
+        render(<SubmitMock />)
 
-describe('VERIFICAÇÃO SE O FORMULARIO ESTA SENDO SUBMETIDO', () => {
+        await userEvent.type(screen.getByRole('textbox'), 'escolaTeste');
+        
+        fireEvent.click(screen.getByRole('button'));
 
-    it('', () => {
-        render(<></>)
+        expect(consoleOutput).toContain(JSON.stringify({ escola: 'escolaTeste' }));
+
+        console.log = originalLog;
     })
-
-
 })
