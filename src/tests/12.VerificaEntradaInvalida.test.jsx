cf8b3faeeaf535728676d95fe7cc289ec1cc4bbb
rinaldo <rabelorinaldo@hotmail.com>
2024-05-21 09:56:43 -0300

    test(form): teste de entrada invalida criado

--- src/tests/12.VerificaEntradaInvalida.test.jsx
+++ src/tests/12.VerificaEntradaInvalida.test.jsx
@@ -6,8 +6,22 @@ import { toast } from "react-toastify";
 
 describe('VERIFICAÇÃO SE O CAMPO ESTÁ RECEBENDO UM DADO INVÁLIDO', () => {
 
-    it('', () => {
-        render(<Form />)
+    it('SERÁ VALIDADO SE OS CAMPOS DE INT RECEBEM STRING (CNPJ E CEP)', async () => {
+        const {getByTestId} = render(<Form />);
+
+        const campos = ['escola', 'cnpj', 'bairro', 'logradouro', 'cep', 'tipoEscola', 'email']
+
+        campos.forEach(campo => {
+            const input = getByTestId(campo) 
+            fireEvent.change(input, { target: { value: 'TESTE' } })
+          })
+
+        fireEvent.click(getByTestId('salvar'));
+
+        await (() => {
+            const toastMessage = toast.isActive(toast.id) ? toast.message : null
+            expect(toastMessage).to.equal('Ocorreu um erro ao enviar os dados.')
+          })
     })
 
 
